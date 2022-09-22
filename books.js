a581ba4f4afa4b1608844327813f552f176d45b9
hopeforkoami <hopeforkoami@gmail.com>
2022-09-22 21:31:01 +0000

    Add js part

--- books.js
+++ books.js
@@ -20,10 +20,17 @@ class BookPage {
   refreshBookList() {
     // update the innerhtml value of books
     let response = '';
-    this.booklist.forEach((book, index) => {
-      response += book.generateBookLi(index);
-    });
-    document.querySelector('.books').innerHTML = response;
+    console.log(this.booklist.length);
+    if(this.booklist.length>0){
+        this.booklist.forEach((book, index) => {
+          response += book.generateBookLi(index);
+        });
+        document.querySelector('.books').innerHTML = response;
+    }
+    else{
+      console.log('no data');
+      document.querySelector('.books').classList.toggle('hidden_item');
+    }
   }
 
   generateId() {
@@ -34,6 +41,40 @@ class BookPage {
 const page = new BookPage();
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
+function displayComponent (elmnts){
+  if(elmnts.isArray){
+    elmnts.forEach(elmnt => {
+      test = elmnt.classList;
+      test.contains('hidden_item')?test.remove('hidden_item'):true;
+    });
+  }
+  else{
+    test = elmnts.classList;
+    test.contains('hidden_item')?test.remove('hidden_item'):true;
+  }
+  
+}
+function HideComponent (elmntsParam){
+  elmnts = Array.from(elmntsParam);
+  /*console.log(elmnts.isArray);
+  if(elmnts.isArray){
+    elmnts.forEach(elmnt => {
+      test = elmnt.classList;
+      test.contains('hidden_item')?true:test.toggle('hidden_item');
+    });
+  }
+  else{
+    test = elmnts.classList;
+    console.log(elmnts);
+    test.contains('hidden_item')?true:test.toggle('hidden_item');
+  }*/
+  elmnts.forEach(elmnt => {
+    test = elmnt.classList;
+    test.contains('hidden_item')?true:test.toggle('hidden_item');
+  });
+  
+  
+}
 
 function addnewbook(e) {
   e.preventDefault();
@@ -41,14 +82,22 @@ function addnewbook(e) {
   page.refreshBookList();
   title.value = '';
   author.value = '';
+  displayComponent(document.querySelector('.books'));
+  HideComponent([document.querySelector('.nodata_Message')]);
 }
 
 function removeBook(index) {
   if (index >= 0) {
     page.remove(index);
     page.refreshBookList();
+    if(page.booklist.length===0){
+      displayComponent(document.querySelector('.nodata_Message'));
+      HideComponent([document.querySelector('.books')]);
+
+    }
   }
 }
+
 removeBook(-2);
 document.querySelector('.submit_button').addEventListener('click', addnewbook);
 window.removeBook = removeBook;
--- general.js
+++ general.js
@@ -1,11 +1,134 @@
 const listMenu = document.querySelector('.book_list_section');
 const  addMenu= document.querySelector('.book_add_section');
 const contactMenu = document.querySelector('.book_contact_section');
+const menuElements = [listMenu,addMenu,contactMenu];
+function setActive(index){
+  menuElements.forEach((element,key) => {
+    if(key==index){
+      element.classList.contains('nav-link-visited')?true:element.classList.toggle('nav-link-visited');
+    }
+    else{
+      element.classList.contains('nav-link-visited')?element.classList.remove('nav-link-visited'):true;
+    }
+  });
+}
+
+function displayComponent (elmnts){
+  if(elmnts.isArray){
+    elmnts.forEach(elmnt => {
+      test = elmnt.classList;
+      test.contains('hidden_item')?test.remove('hidden_item'):true;
+    });
+  }
+  else{
+    test = elmnts.classList;
+    test.contains('hidden_item')?test.remove('hidden_item'):true;
+  }
+  
+}
+function HideComponent (elmntsParam){
+  elmnts = Array.from(elmntsParam);
+  /*console.log(elmnts.isArray);
+  if(elmnts.isArray){
+    elmnts.forEach(elmnt => {
+      test = elmnt.classList;
+      test.contains('hidden_item')?true:test.toggle('hidden_item');
+    });
+  }
+  else{
+    test = elmnts.classList;
+    console.log(elmnts);
+    test.contains('hidden_item')?true:test.toggle('hidden_item');
+  }*/
+  elmnts.forEach(elmnt => {
+    test = elmnt.classList;
+    test.contains('hidden_item')?true:test.toggle('hidden_item');
+  });
+  
+  
+}
 
 function showbooklist(e) {
   e.preventDefault();
-  document.querySelector('.add_book_section').classList.toggle('hidden_item');
-  document.querySelector('.contact').classList.toggle('hidden_item');
-}
-listMenu.addEventListener('click', showbooklist);
+  displayComponent(document.querySelector('.booklistsection'));
+  HideComponent([
+    document.querySelector('.add_book_section'),
+    document.querySelector('.separator'),
+    document.querySelector('.contact')
 
+  ]);
+  setActive(0);
+
+  
+}
+function showAddForm(e) {
+  e.preventDefault();
+  displayComponent(document.querySelector('.add_book_section'));
+  HideComponent([
+    document.querySelector('.contact'),
+    document.querySelector('.booklistsection')
+
+  ]);
+  setActive(1);
+}
+function showContact(e) {
+  e.preventDefault();
+  displayComponent(document.querySelector('.contact'));
+  HideComponent([
+    document.querySelector('.add_book_section'),
+    document.querySelector('.booklistsection')
+  ]);
+  setActive(2);
+}
+
+listMenu.addEventListener('click', showbooklist);
+addMenu.addEventListener('click', showAddForm);
+contactMenu.addEventListener('click', showContact);
+function generateDateEnd(day){
+  if((day>=10)&&(day<=20))
+  {
+    return "th"
+  }
+  else{
+    switch(day%10){
+    case 1: return "st";
+    case 2: return "nd";
+    case 3: return "rd";
+    default: return "th";
+
+  }
+  }
+  
+  
+}
+
+function generateFormatedHour(actualdate){
+  hour = actualdate.getHours();
+  if(hour===12){
+    return hour+":"+actualdate.getMinutes()+":"+actualdate.getSeconds()+" am";
+  }
+  else if(hour===24){
+    return "12:"+actualdate.getMinutes()+":"+actualdate.getSeconds()+" pm";
+  }
+  else{
+    if(hour<12){
+      return hour+":"+actualdate.getMinutes()+":"+actualdate.getSeconds()+" am";
+    }
+    else{
+      return (hour%12)+":"+actualdate.getMinutes()+":"+actualdate.getSeconds()+" pm";
+    }
+  }
+}
+function displayDateTime() 
+{
+    let actualDate = new Date();
+    let options = { year: "numeric", month: "long", day: "2-digit"};
+    let date = actualDate.toLocaleDateString("us-US", options);
+    let time = ("0" + actualDate.getHours()).slice(-2) + ":" + ("0" + actualDate.getMinutes()).slice(-2) + ":" + ("0" + actualDate.getSeconds()).slice(-2);
+    let dateTime = date + " " + time;
+    console.log();
+    reformat = date.split(",");
+    let dayend = generateDateEnd(actualDate.getDate())
+    document.querySelector('.date').innerHTML =  reformat[0]+dayend+", "+reformat[1]+"  "+ generateFormatedHour(actualDate);
+}
+setInterval(displayDateTime, 1000);
